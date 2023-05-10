f4ab4fce2085ddaae6959ffa1a34cc996160d220
Wesley Blackburn <wesley.blackburn90@yahoo.com>
2023-05-10 16:29:14 -0400

    added user auth

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,63 @@
 const express = require('express')
 const router = express.Router()
+const gravatar = require('gravatar')
+const bcrypt = require('bcryptjs')
+const {check, validationResult} = require('express-validator')
+
+const User = require('../../models/User')
+
+router.post('/', [
+  check('name', 'Name is required').not().isEmpty(),
+  check('email', 'Please include a valid email address').isEmail(),
+  check('password', 'Please enter a password with 6 or more characters').isLength({min:6})
+], async (req,res) => {
+  const errors = validationResult(req)
+  if(!errors.isEmpty()) {
+    return res.status(400).json({errors: errors.array()})
+  }
+
+  //see if user exists
+
+  const {name, email, password} = req.body
+
+  try {
+    let user = await User.findOne({email})
+    if(user) {
+      res.status(400).json({errors: [{msg: "User already exists"}]})
+    }
+
+    //get users gravatar
+    const avatar = gravatar.url(email, {
+      s: '200',
+      r: 'pg',
+      d: 'mm'
+    })
+
+    user = new User ({
+      name,
+      email,
+      avatar,
+      password
+    })
+
+    //encrypt password
+    const salt = await bcrypt.genSalt(10)
+
+    user.password = await bcrypt.hash(password, salt)
+
+    await user.save()
+
+    //return jsonwebtoken
 
 
-router.get('/', (req,res) => {
-  res.send('User route')
+    res.send('User registered')
+  } catch(err) {
+    console.error(err.message)
+    res.status(500).send("Server error")
+  }
+
+
+
 })
 
 module.exports = router;
--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const app = express()
 //connect to database
 connectDB()
 
+app.use(express.json({extended: false }))
+
 app.get('/', (req, res) => res.send('API running'))
 
 app.use('/api/users', require("./routes/api/users"))
